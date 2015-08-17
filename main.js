762c44d2ec0a4bd39fa51cbf99f3888270fc6a49
Greg Pynes Pynes <gpynes@users.noreply.github.com>
2015-08-17 11:09:14 -0700

    Added winer function and changed li's to divs

--- main.js
+++ main.js
@@ -1,47 +1,62 @@
-// document.querySelector("#topLeft").addEventListener( "click", function() {
-// 	document.getElementById("#leftTop").innerHTML = "X";
-// });
-
 var player = true;
+var wins = [ [ 0, 1, 2 ], [ 3, 4, 5 ], [ 6, 7, 8 ], [ 0, 3, 6 ], [ 1, 4, 7 ], [ 2, 5, 8 ], [ 0, 4, 8 ], [ 2, 4, 4 ] ];
 
 function setPlayer() {
 	return ( player ) ? "X" : "O";	
 }
 
 function isWin () {
-
+	var cnt = 0;
+	for ( var i = 0; i < wins.length; i++ ) {
+		cnt = 0;
+		for ( var j = 0; j < wins[ i ].length; j++ ) {
+			if ( boxArray[ wins[ i ][ j ] ].innerText === setPlayer() ) {
+				console.log( setPlayer() );
+				cnt++;
+			}
+		}
+		if ( cnt >= 3 ) {
+			return true;
+		}
+	}
+	return false;
 }
 
 function isOpen( stuff ) {
 	return !stuff;
 }
 
-var boxArray = document.querySelectorAll( "li" );
-
-for ( var i = 0 ; i < boxArray.length; i++ ) {
-	boxArray[ i ].addEventListener( "click",function() {
-		document.getElementsByTagName( "h1" )[ i ].innerHTML = "X";
-	} );
+function resetBoard() {
+	for ( var i = 0; i < boxArray.length; i++ ) {
+		boxArray[ i ].inerText = undefined;
+	}
 }
 
+var boxArray = document.querySelectorAll( ".square" );
+
+    for ( var i = 0 ; i < boxArray.length; i++ ) {
+ 	boxArray[ i ].addEventListener( "click",function() {
 
 
-// if ( isOpen( this.children[ 0 ].innerText ) ) {
-// 			 this.children[ 0 ].innerText = setPlayer();	 
-// 			 player = !player;
-// 		} else { 
-// 			alert("That space has been taken!");
-// 		}
+    	if ( isOpen( this.children[ 0 ].innerText ) ) {
+			this.children[ 0 ].innerText = setPlayer();
+			if ( isWin() ) {
+				alert(setPlayer() + " is the winner!" );
+				resetBoard();
+			}
+			player = !player;
+		} 
+		else { 
+			alert("That space has been taken!");
+		}
+	}
+)}
 
-// boxArray[ 0 ].addEventListener( "click", function() {
-// 	if ( player % 2 ) {
-// 		document.getElementById( "1" ).innerText = "X";
-// 		player++;
-// 	} else {
-// 		document.getElementById( "1" ).innerText = "O";
-// 		player++;
-// 	}
-// })
+
+
+
+
+
 
 
 
